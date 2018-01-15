98aaa5ce4113bb8dd2a1b5c79aa7a9e985990b5a
MAChitgarha <machitgarha@outlook.com>
2018-01-15 16:45:35 +0330

    Initial commit
    Create calculator. Its operations contains summation, subtraction,
    multiplication and division. It supports float pointing numbers and has
    the ability of clearing characters one by one. Also, the monitor of it
    scrolls really nice.
    
    Used libraries/frameworks/plugins:
    jQuery
    MouseWheel plugin for jQuery

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html dir="ltr">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="index.css"/>
+    <title>Calculator</title>
+</head>
+<body>
+    <section id="calculator">
+        <section id="res">
+            <section></section>
+        </section>
+        <section>
+            <table id="keys">
+                <tr>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>/</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>*</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>←</td>
+                    <td>.</td>
+                    <td>=</td>
+                    <td>+</td>
+                </tr>
+            </table>
+        </section>
+    </section>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.mousewheel.js"></script>
+    <script src="js/calculator.js"></script>
+</body>
+</html>
---
+++ js/calculator.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    let $res = $("section#res section"),
+        $tableKeys = $("table#keys td"),
+        lastChar = null,
+        calc = "";
+
+    $res.on("mousewheel", function (event) {
+        let dx = event.deltaX,
+            fac = event.deltaFactor;
+        if (dx !== 0)
+            $(this).scrollLeft(dx * fac + $(this).scrollLeft());
+    });
+
+    $tableKeys.on("click", function () {
+        let curChar = $(this).text();
+
+        switch (curChar) {
+            case "=":
+                $res.text(calc = eval(calc).toString());
+                break;
+
+            case "←":
+                $res.text(calc = calc.slice(0, -1));
+                break;
+
+            default:
+                let isLastNum = parseInt(lastChar).toString() === lastChar,
+                    isCurNum = parseInt(curChar).toString() === curChar;
+
+                if (curChar === ".")
+                    isCurNum = true;
+                if (lastChar === ".")
+                    isLastNum = true;
+
+                if (!isLastNum && !isCurNum)
+                    $res.text(calc = calc.slice(0, -2));
+
+                calc += (isCurNum && isLastNum && lastChar !== null ? "" : " ")
+                    + curChar;
+
+                $res.text(calc);
+                lastChar = curChar;
+
+                break;
+        }
+    });
+});
